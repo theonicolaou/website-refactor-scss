102679cb9d76d5ffeda147f32ef3fb2d3089996d
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2014-12-24 19:32:08 +0000

    Initial commit of Gruntfile, README and SCSS folder

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+			grunt.initConfig({
+  		
+  		sass: {
+    		dist: {
+      		files: {
+        		'css/tn.css': 'scss/tn.scss',
+        		'css/tn-mediaqueries.css': 'scss/tn-mediaqueries.scss'
+      		},
+      		options: {
+      			sourcemap: 'none'
+      		},
+      	}
+  		},
+  		
+  		watch: {
+    		sass: {
+      		files: ['scss/*/*.scss'],
+      		tasks: ['sass'],
+    		},
+    	},
+    	
+    	notify_hooks: {
+    		options: {
+      		enabled: true,
+      		title: "Grunt tasks completed",
+      		success: true,
+      		duration: 5
+    		}
+  		}	  		
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
+
+	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.task.run('notify_hooks');
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Refactored SCSS for www.theo-nicolaou.co.uk</h1>
+These files are the (mostly final) SCSS for my website. The Grunt file is quite basic - it just compiles and watches the SCSS folder. Install the node modules using "npm install" and then run "grunt" to see the generated CSS.
